61fceb4a3b0451832a282dd06edf8c8c7fe993fb
AryaaSatya <yudistiraarya819@gmail.com>
2024-09-24 14:17:59 +0700

    Add files via upload
    objek 1

---
+++ Objek1/src/main/java/com/dicoding/javafundamental/objek1/Objek1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.dicoding.javafundamental.objek1;
+
+/**
+ *
+ * @author VICTUS
+ */
+public class Objek1 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama hewan: Elang");
+    }
+}
