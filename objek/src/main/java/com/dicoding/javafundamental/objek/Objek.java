3e77761c77d87ec9b1c9122a279bf5169ae97bd4
AryaaSatya <yudistiraarya819@gmail.com>
2024-09-24 13:49:12 +0700

    Add files via upload

---
+++ objek/src/main/java/com/dicoding/javafundamental/objek/Objek.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.dicoding.javafundamental.objek;
+
+/**
+ *
+ * @author VICTUS
+ */
+public class Objek {
+
+    public static void main(String[] args) {
+        System.out.println("Nama Hewan: Elang");
+    }
+}
